fa816883bd5e6e6b93a11654e7befdce9f632ce6
BadrSayed2 <BadrSayed2@example.com>
2026-06-15 15:28:31 +0300

    Add SearchScreen with search functionality and FriendContainerWidget

---
+++ lib/features/search/presentation/view/screens/search_screen.dart
@@ -0,0 +1,70 @@
+import 'package:chat_app/core/utils/app_colors.dart';
+import 'package:chat_app/features/home/presentation/view/widgets/freind_container_widget.dart';
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+  static const String route = "SearchScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        centerTitle: true,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset("assets/images/scholar.png", width: 50, height: 50),
+            Text(
+              "Chat",
+              style: TextStyle(
+                color: AppColors.textColor,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "search on your friends",
+                suffixIcon: Icon(Icons.send, color: AppColors.primaryColor),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(28),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(28),
+                  borderSide: BorderSide(color: AppColors.primaryColor),
+                ),
+              ),
+            ),
+          ),
+          FriendContainerWidget(
+            friendName: "mohamed Romdan",
+            lastMessage: "يا صاحبي خلصت الكود ولا لسه؟",
+            time: "10:30 PM",
+            onTap: () {},
+          ),
+          // EmptyFreinds(),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+          // Expanded(
+          //   child: ListView.builder(
+          //     itemBuilder: (context, index) {
+          //       return;
+          //     },
+          //   ),
+          // ),
