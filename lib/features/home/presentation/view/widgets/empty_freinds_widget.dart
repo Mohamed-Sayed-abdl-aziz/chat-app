e4fca71b5ab55bf63706c211693867a144df9b26
BadrSayed2 <BadrSayed2@example.com>
2026-06-15 14:45:48 +0300

    Implement HomeScreen layout with AppBar, search field, and FriendContainerWidget; add EmptyFriends widget

---
+++ lib/features/home/presentation/view/widgets/empty_freinds_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class EmptyFreinds extends StatelessWidget {
+  const EmptyFreinds({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Text(
+          "No chats founded",
+          style: TextStyle(
+            color: Color(0xff000000),
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+}
