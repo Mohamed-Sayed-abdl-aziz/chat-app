f0ee5b8f7fdec8710dfda805335a03a8c4549b34
BadrSayed2 <BadrSayed2@example.com>
2026-06-21 17:31:05 +0300

    Refactor chat functionality: implement ChatCubit, add use cases for sending and retrieving messages, and update ChatScreen with state management

--- lib/features/chat/domain/use_case/send_messages_use_case.dart
+++ lib/features/chat/domain/use_case/send_messages_use_case.dart
@@ -1,14 +1,7 @@
 import 'package:chat_app/core/network/resulet_firebase.dart';
+import 'package:chat_app/features/chat/data/repo/repository/chat_repository_imp.dart';
 import 'package:chat_app/features/chat/domain/entity/message_entity.dart';
 import 'package:chat_app/features/chat/domain/repo/repository/chat_repository.dart';
-
-class GetMessagesUseCase {
-  GetMessagesUseCase({required ChatRepository repo}) : _repo = repo;
-
-  final ChatRepository _repo;
-  Stream<ResultFirebase<List<MessageEntity>>> invoke(String chatId) =>
-      _repo.getMessages(chatId);
-}
 
 class SendMessageUseCase {
   SendMessageUseCase({required ChatRepository repo}) : _repo = repo;
@@ -20,3 +13,7 @@ class SendMessageUseCase {
     List<String> chatMembers,
   ) => _repo.sendMessage(chatId, message, chatMembers);
 }
+// chatRepositoryInjectable
+
+SendMessageUseCase sendMessageUseCaseInjectable() =>
+    SendMessageUseCase(repo: chatRepositoryInjectable());
---
+++ lib/features/chat/presentation/view_model/chat_cubit.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:chat_app/core/network/resulet_firebase.dart';
+import 'package:chat_app/features/chat/domain/entity/message_entity.dart';
+import 'package:chat_app/features/chat/domain/use_case/get_messages_use_cse.dart';
+import 'package:chat_app/features/chat/domain/use_case/send_messages_use_case.dart';
+import 'package:meta/meta.dart';
+
+part 'chat_state.dart';
+
+class ChatCubit extends Cubit<ChatState> {
+  ChatCubit({
+    required GetMessagesUseCase getMessages,
+    required SendMessageUseCase sendMessage,
+  }) : _getMessages = getMessages,
+       _sendMessage = sendMessage,
+       super(ChatInitial());
+  final GetMessagesUseCase _getMessages;
+  final SendMessageUseCase _sendMessage;
+  StreamSubscription? _getMessagesSubscription;
+  void getMessages(String chatId) {
+    emit(ChatLoading());
+    _getMessagesSubscription?.cancel();
+    _getMessagesSubscription = _getMessages.invoke(chatId).listen((resulte) {
+      switch (resulte) {
+        case Success<List<MessageEntity>>():
+          if (resulte.data.isEmpty) {
+            emit(ChatEmpty());
+          } else {
+            emit(GetMessagesSuccess(messages: resulte.data));
+          }
+        case Error<List<MessageEntity>>():
+          emit(ChatError(error: resulte.error));
+      }
+    });
+  }
+
+  Future<void> sendMessage(
+    String chatId,
+    MessageEntity message,
+    List<String> chatMembers,
+  ) async {
+    final resulte = await _sendMessage.invoke(chatId, message, chatMembers);
+    switch (resulte) {
+      case Success<bool>():
+        emit(SendMessageSuccess());
+      case Error<bool>():
+        emit(ChatError(error: resulte.error));
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _getMessagesSubscription?.cancel();
+    return super.close();
+  }
+}
